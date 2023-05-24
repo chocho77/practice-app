e8d586e032307c2ba8ed4a9a88a7a65df10d5ed8
chavdar.momchilov <chavmom@gmail.com>
2023-05-24 10:38:38 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/app/app.component.html
    	modified:   src/app/maincontent/maincontent.component.ts
    	modified:   src/app/navbar/navbar.component.ts
    	modified:   src/styles.css

--- src/app/maincontent/maincontent.component.ts
+++ src/app/maincontent/maincontent.component.ts
@@ -2,8 +2,17 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'app-maincontent',
-  template:'<h1>Maincontent component</h1>',
-  styles:['h1{color:green}']
+  template:`<div>
+           <h1>Title</h1>
+           <main>Main Content
+            <h2>Header 2</h2>
+           </main>
+           </div>`,
+  styles:[`h1 {color:red}
+           div {background-color:green}
+           div{min-height:600px} 
+           
+           `]
 })
 export class MaincontentComponent {
   
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,16 @@ import {Component} from '@angular/core';
 
 @Component({
   selector:'app-navbar',
-  template: '<h1>Navbar Component</h1>',
-  styles:['h1{color: red}']
+  template: `<div>
+             <main>
+              <h1>Navbar Component</h1>
+             </main>
+            </div>`,
+  styles:[`div {background-color:green}
+           div {min-height:400px}
+           main {background-color:yellow}
+           h1{color:blue}
+           `]
 
 })
 export class NavbarComponent {
